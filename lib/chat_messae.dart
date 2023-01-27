aad71bd10858df402b7aace4cb51e4c1b32e50c9
soikat77 <soikatalam26@gmail.com>
2023-01-27 21:43:32 +0600

    ok

---
+++ lib/chat_messae.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final String sender;
+
+  const ChatMessage({super.key, required this.text, required this.sender});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(right: 12.0),
+          child: CircleAvatar(child: Text(sender[0])),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                sender,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,15 +1,73 @@
+import 'dart:async';
+
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'chat_messae.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _chatController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  ChatGPT? chatGPT;
+
+  StreamSubscription? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    chatGPT = ChatGPT.instance;
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    ChatMessage message =
+        ChatMessage(text: _chatController.text, sender: "user");
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    _chatController.clear();
+
+    final request = CompleteReq(
+      prompt: message.text,
+      model: kTranslateModelV3,
+      max_tokens: 200,
+    );
+    _subscription = chatGPT!
+        .builder('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')
+        .onCompleteStream(request: request)
+        .listen((response) {
+      ChatMessage botMessage =
+          ChatMessage(text: response?.choices[0].text ?? " ", sender: 'Bot');
+
+      setState(() {
+        _messages.insert(0, botMessage);
+      });
+    });
+  }
 
   Widget _buildTextComposer() {
     return Row(
       children: [
-        const Expanded(
+        Expanded(
           child: TextField(
-            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            controller: _chatController,
+            onSubmitted: (value) => _sendMessage(),
+            decoration:
+                const InputDecoration.collapsed(hintText: "Send a message"),
           ),
         ),
         IconButton(
@@ -26,14 +84,30 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat Bot'),
       ),
-      body: Column(children: [
-        Container(
-          decoration: BoxDecoration(
-            color: context.cardColor,
+      body: SafeArea(
+        child: Column(children: [
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              padding: Vx.m8,
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return _messages[index];
+                // return Container(
+                //   height: 50,
+                //   color: Colors.red,
+                // ).p12();
+              },
+            ),
           ),
-          child: _buildTextComposer(),
-        )
-      ]),
+          Container(
+            decoration: BoxDecoration(
+              color: context.cardColor,
+            ),
+            child: _buildTextComposer(),
+          )
+        ]),
+      ),
     );
   }
 }
